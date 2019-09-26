839b62ad7c8f617d68d7ffc8a57eeec5ba442d05
zymfe <1047832475@qq.com>
2019-09-26 17:53:46 +0800

    fix: cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,19 +1,17 @@
 module.exports = async (ctx, next) {
-  app.use(async (ctx, next) => {
-    const allowedOrigin = [
-      'https://web.0351zhuangxiu.com',
-      'http://localhost:8080'
-    ];
+  const allowedOrigin = [
+    'https://web.0351zhuangxiu.com',
+    'http://localhost:8080'
+  ];
 
-    if (allowedOrigin.includes(ctx.header.origin)) {
-      ctx.set('Access-Control-Allow-Origin', '*');
-    }
+  if (allowedOrigin.includes(ctx.header.origin)) {
+    ctx.set('Access-Control-Allow-Origin', '*');
+  }
 
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers');
-    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, HEAD');
-    ctx.set('Content-Type', 'application/json; charset=utf-8');
-    ctx.status = 200;
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, HEAD');
+  ctx.set('Content-Type', 'application/json; charset=utf-8');
+  ctx.status = 200;
 
-    await next();
-  });
+  await next();
 }
