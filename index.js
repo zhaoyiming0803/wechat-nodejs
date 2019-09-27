ad9c3ea3ed58cc1c25a7514eca26c28702514e5b
zymfe <1047832475@qq.com>
2019-09-27 13:50:14 +0800

    update

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 const router = require('./router');
 const cors = require('./middleware/cors');
-const bodyParse = require('koa-bodyparser');
 
-app.use(bodyParse);
 app.use(cors);
 app.use(router.routes(), router.allowedMethods()); 
 
--- middleware/cors.js
+++ middleware/cors.js
@@ -8,7 +8,7 @@ module.exports = async (ctx, next) => {
     ctx.set('Access-Control-Allow-Origin', '*');
   }
 
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers');
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Expose-Headers, Token, Platform');
   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, HEAD');
   ctx.set('Content-Type', 'application/json; charset=utf-8');
   ctx.status = 200;
