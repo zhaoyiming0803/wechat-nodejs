7c8db5ed752561528161bf498981867d01ff9353
zymfe <1047832475@qq.com>
2019-09-27 15:49:57 +0800

    update Message

--- wechat/Message.js
+++ wechat/Message.js
@@ -5,35 +5,85 @@ const getRawBody = require('raw-body');
 const { parseXML, createXMLMessage } = require('../util');
 
 module.exports = class Message {
-  constructor () {
-    
-  }
-
   async init (ctx) {
-    this.ctx = ctx;
     const data = await getRawBody(ctx.req, {
       length: ctx.length,
       limit: '1mb',
       encoding: ctx.charset
     });
     const content = await parseXML(data);
-    const [MsgType] = content.xml.MsgType;
+    const message = createXMLMessage(content.xml);
+    const { MsgType }  = message;
     const handler = 'handle' + MsgType.slice(0, 1).toUpperCase() + MsgType.slice(1);
 
-    this[handler](content);
+    this.message = message;
+    this.ctx = ctx;
+    this[handler]();
   }
 
-  handleText (content) {
-    this.putMessage(content, '您刚刚发给我一段文字', 'text');
+  handleText () {
+    this.reply(`您发送的内容是：${this.message.Content}`);
+  }
+
+  handleImage () {
+    this.reply('您发送了一张图片');
+  }
+
+  handleVoice () {
+    this.reply('您发送了一段语音');
+  }
+
+  handleVideo () {
+    this.reply('您发送了一段视频');
+  }
+
+  handleShortvideo () {
+    this.reply('您发送了一段短视频');
+  }
+
+  handleLocation () {
+    this.reply('您发送了地理位置');
+  }
+
+  handleLink () {
+    this.reply('您发送了一条链接');
   }
 
   handleEvent () {
+    const eventMap = {
+      subscribe () {
+        this.reply('感谢关注我的测试公众号，可以加我个人微信号：1047832475，或点击菜单 GitHub，查看我的开源项目^_^');
+      },
 
+      unsubscribe () {
+        console.log('上报：某某用户取消了关注');
+      },
+
+      SCAN () {
+        console.log('上报，扫描带参数的二维码，做其他业务逻辑处理');
+      },
+
+      LOCATION () {
+        console.log('用户上报了地理位置');
+      },
+
+      CLICK () {
+        this.reply('您刚刚点击了拉取消息的菜单按钮');
+      },
+
+      VIEW () {
+        this.reply('您刚刚点击了菜单跳转链接');
+      }
+    }
+
+    eventType[this.message.Event]()
   }
 
-  putMessage (content, returnMsg, type) {
-    const ctx = this.ctx;
-    const message = createXMLMessage(content.xml);
+  /**
+   * 暂时全部回复文本消息，后期有时间再拓展
+   */
+  reply (returnMsg) {
+    const { ctx, message } = this;
     
     ctx.status = 200;
     ctx.type = 'application/xml';
@@ -41,7 +91,7 @@ module.exports = class Message {
       <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
       <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
       <CreateTime>${parseInt(Date.now()/10, 10)}</CreateTime>
-      <MsgType><![CDATA[${type}]]></MsgType>
+      <MsgType><![CDATA[text]]></MsgType>
       <Content><![CDATA[${returnMsg}]]></Content>
       <MsgId>${message.MsgId}</MsgId>
     </xml>`;
